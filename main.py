9f2137684e1a1810bbd820184c896a583605fea0
Naikelvivek <naikelvivek@gmail.com>
2025-11-02 11:24:03 +0530

    main added

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+from src.textSummarizer.logging import logger
+
+logger.info("Starting the text summarization process.")
