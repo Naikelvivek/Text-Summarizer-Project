5ef350eb416cd877d3f1ccf9b1d594eb1905564e
Naikelvivek <naikelvivek@gmail.com>
2025-11-02 12:32:44 +0530

    research file added

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -6,3 +6,24 @@ from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """Reads a YAML file and returns its contents as a ConfigBox object.
+
+    Args:
+        path_to_yaml (Path): The path to the YAML file.
+
+    Returns:
+        ConfigBox: The contents of the YAML file as a ConfigBox object.
+    """
+    try:
+        with open(path_to_yaml, "r") as yaml_file:
+            content = yaml.safe_load(file)
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("Yaml file is empty")
+    except Exception as e:
+        raise e
+    
+    
